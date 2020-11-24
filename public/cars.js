21aae370d22e66e42963b80b8afe4c6e0e1f2dea
JodyFortuin <jodifortuin@gmail.com>
2020-11-24 16:46:05 +0200

    commit

---
+++ public/cars.js
@@ -0,0 +1,35 @@
+const carsElem = document.querySelector(".cars");
+const carList = document.querySelector(".carList");
+const carsTemplateText = document.querySelector(".carsTemplate").innerHTML;
+const carsTemplate = Handlebars.compile(carsTemplateText);
+
+const colorElem = document.querySelector(".colors");
+const colorList = document.querySelector(".colorList");
+const colorTemplateText = document.querySelector(".colorTemplate").innerHTML;
+const colorTemplate = Handlebars.compile(colorTemplateText);
+
+
+const  allElem= document.querySelector(".allC");
+const allCars = document.querySelector(".allCarList");
+const allCarTemplateText = document.querySelector(".allCarTemplate").innerHTML;
+const allCarTemplate = Handlebars.compile(allCarTemplateText);
+
+//get data in browser
+axios.get("http://api-tutor.herokuapp.com/v1/makes").then(function (result) {
+  console.log(result);
+  carList.innerHTML = carsTemplate({
+    cars: result.data,
+  });
+});
+axios.get("http://api-tutor.herokuapp.com/v1/colors").then(function (result) {
+  console.log(result);
+  colorList.innerHTML = colorTemplate({
+    color: result.data,
+  });
+});
+axios.get("http://api-tutor.herokuapp.com/v1/cars").then(function (result) {
+  console.log(result);
+  allCars.innerHTML = allCarTemplate({
+    all: result.data,
+  });
+});
